e5d64f884de9fb40f49aea12430b9f919b052735
Dmitri Sanzharov <dmitrisanzharov@gmail.com>
2023-05-28 17:41:22 +0100

    aaa

---
+++ 01_output.php
@@ -0,0 +1,16 @@
+<?php
+
+$name = 'Dimi';
+
+// echo $name;
+
+$has_kids = false;
+
+// echo $has_kids;
+
+$age = 30;
+
+// echo $name . ' is ' . $age . ' years old, omg!';
+
+define('MAHMAN', 10);
+echo MAHMAN;
