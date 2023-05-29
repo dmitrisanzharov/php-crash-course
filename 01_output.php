ec800dc726435fe581d596048a359925832d9b09
Dmitri Sanzharov <dmitrisanzharov@gmail.com>
2023-05-29 22:39:47 +0100

    dsfsdf

--- 01_output.php
+++ 01_output.php
@@ -1,16 +1,5 @@
 <?php
 
-$name = 'Dimi';
+$myFun = fn ($arg1, $arg2) => $arg1 + $arg2;
 
-// echo $name;
-
-$has_kids = false;
-
-// echo $has_kids;
-
-$age = 30;
-
-// echo $name . ' is ' . $age . ' years old, omg!';
-
-define('MAHMAN', 10);
-echo MAHMAN;
+echo $myFun(1, 2);
